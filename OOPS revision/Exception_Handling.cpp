a5e252636c07a16d3549c30e9ed7f9cfadf1cc98
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-01-17 06:41:54 +0530

    OOps rivision

--- OOPS revision/Exception_Handling.cpp
+++ OOPS revision/Exception_Handling.cpp
@@ -4,5 +4,44 @@ using namespace std;
 
 int main()
 {
-    
+    try
+    {
+        double balance = 1000;
+        double amount;
+        // deposit
+        cout << "Enter deposit amount : ";
+        cin >> amount;
+
+        if (amount < 0)
+        {
+            cout << "Amount can't be zero" << endl;
+        }
+
+        balance = balance + amount;
+        cout << "Balance in the account : " << balance << endl;
+
+        // withdraw
+        cout << "Enter withdrawl amount : ";
+        cin >> amount;
+        try
+        {
+            if (amount < 0)
+            {
+                throw invalid_argument("amount can't be less than zero");
+            }
+            if(amount > balance){
+                throw runtime_error("insufficient balance");
+            }
+            balance = balance - amount;
+            cout << "balance in the amouunt : " << balance << endl;
+        }
+        catch (exception &e)
+        {
+            cout << e.what();
+        }
+    }
+    catch (exception &e)
+    {
+        cout << e.what();
+    }
 }
